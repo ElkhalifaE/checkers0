b6ff06cb181fdc10a58ea90a4c4d183bc12d5273
Elkhalifa Elhamdi <malikelhamdi@outlook.com>
2022-03-31 15:10:16 -0500

    listen

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express")
 const app = express()
 
-app.use(express.static(__dirname + "/public"));
-
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/public/index.html")
-})
-
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 
 app.listen(port);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/public/index.html")
+})
+
