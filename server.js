216b123543296da354154fbfc35ff1b53dc7a73b
Elkhalifa Elhamdi <malikelhamdi@outlook.com>
2022-03-31 15:09:04 -0500

    Added Procfile

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/public/index.html")
 })
 
-app.listen(process.env.PORT || 3000)
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port);
